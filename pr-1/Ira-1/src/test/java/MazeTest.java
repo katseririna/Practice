913d00806201923661ae6f7a892da28cc13261b4
katseririna <katser.ir@yandex.ru>
2018-07-08 12:40:20 +0300

    test
    test

---
+++ pr-1/Ira-1/src/test/java/MazeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MazeTest {
+
+    @Test
+    public void on_mouse_dragged() {
+    }
+
+    @Test
+    public void paintComponents() {
+    }
+
+    @Test
+    public void set_delay() {
+    }
+
+    @Test
+    public void paint() {
+    }
+}
